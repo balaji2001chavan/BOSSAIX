f7c942c47727a064a107f72b6705f9e0ec0689e4
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-11-18 03:13:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`BossAIX Server Running on port ${PORT}`);
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });

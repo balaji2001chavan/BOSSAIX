33f0599a4f3ec7b01e8a867b1ab8b21f8f82b039
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-11-17 13:55:38 +0530

    Update UniverseEngine.js

--- src/UniverseEngine.js
+++ src/UniverseEngine.js
@@ -1,3 +1,27 @@
+// GALAXY BACKGROUND (simple stars)
+const starGeometry = new THREE.BufferGeometry();
+const starCount = 2000;
+const starPositions = [];
+
+for (let i = 0; i < starCount; i++) {
+  const x = (Math.random() - 0.5) * 2000;
+  const y = (Math.random() - 0.5) * 2000;
+  const z = (Math.random() - 0.5) * 2000;
+  starPositions.push(x, y, z);
+}
+
+starGeometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(starPositions, 3)
+);
+
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff,
+  size: 2
+});
+
+const stars = new THREE.Points(starGeometry, starMaterial);
+scene.add(stars);
 import * as THREE from "three";
 
 export function runUniverseEngine(mount) {

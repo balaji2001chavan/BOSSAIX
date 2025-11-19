6593018bf1b293a82dd17ee87c592db11be2a50b
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-11-19 22:42:33 +0530

    Create page.js

---
+++ frontend/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      background: "#000",
+      color: "#00f6ff",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "32px"
+    }}>
+      BOSS AIX PLATFORM UI LOADED 🔥
+    </div>
+  );
+}

897e3f623a4854a0010c35e1c8942e7d28f51517
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-11-19 23:04:23 +0530

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,15 +1,5 @@
+import ChatUI from "../components/ChatUI";
+
 export default function Home() {
-  return (
-    <div style={{
-      background: "#000",
-      color: "#00f6ff",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: "32px"
-    }}>
-      BOSS AIX PLATFORM UI LOADED 🔥
-    </div>
-  );
+  return <ChatUI />;
 }

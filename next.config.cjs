5ebf1731a2782a9f862e4a32416e618d970a5922
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-11-20 23:49:22 +0530

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   reactStrictMode: false,
   experimental: {
     serverActions: true,
-  },
+  }
 };
 
 export default nextConfig;

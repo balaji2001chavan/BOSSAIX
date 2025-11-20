66fbf86430c0cc0f813db9343c2dabc56f8f78c2
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-11-20 21:47:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  experimental: {
-    outputStandalone: true
-  }
+module.exports = {
+  output: 'standalone',
+  reactStrictMode: true,
 };
-
-export default nextConfig;
